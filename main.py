c1f65f30aec17cc68e2af3a7cbf5eeb4cb2b4af9
TIL-MichalMioduchowski <michal.mioduchowski@theinformationlab.co.uk>
2026-05-05 09:35:26 +0100

    Bikepoints Extract Cleaned Up in Prep for Load

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ import os
 from datetime import datetime
 import time
 
-timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
 url = f'https://api.tfl.gov.uk/BikePoint'
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 response = requests.get(url)
+data = response.json()
 
 if response.status_code == 200:
-    data = response.json()
-    folder = 'bikepoints'
 
-    os.makedirs(folder, exist_ok=True)
+    dir = 'data'
+    os.makedirs(dir, exist_ok=True)
+    filename = f'{dir}/{timestamp}.json'
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
-    for bp in data:
-        bp_id = bp['id']
-        filename = os.path.join(folder, f'{bp_id}.json')
-        with open(filename, 'w') as file:
-            json.dump(bp, file)
-        print(f'File {filename} was successfully created!')
+    print(f'File {filename} was succesfully created.')
+
 else:
-    print(f'Error:{response.status_code}')
+    print(f'Error: {response.status_code} {data.get("message", "no message found")}')
