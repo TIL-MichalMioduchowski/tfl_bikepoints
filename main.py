4c64f0ae360cce992fa44eeec3dc62db6c8992ab
TIL-MichalMioduchowski <michal.mioduchowski@theinformationlab.co.uk>
2026-05-05 10:04:11 +0100

    Retries Added

--- main.py
+++ main.py
@@ -3,21 +3,33 @@ import json
 import os
 from datetime import datetime
 import time
+import logging
 
 url = f'https://api.tfl.gov.uk/BikePoint'
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 response = requests.get(url)
 data = response.json()
 
-if response.status_code == 200:
+count = 0
+max_tries = 3
 
-    dir = 'data'
-    os.makedirs(dir, exist_ok=True)
-    filename = f'{dir}/{timestamp}.json'
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+while count < max_tries:
+    if 20 <= response.status_code < 300:
 
-    print(f'File {filename} was succesfully created.')
+        dir = 'data'
+        os.makedirs(dir, exist_ok=True)
+        filename = f'{dir}/{timestamp}.json'
+        with open(filename, 'w') as file:
+            json.dump(data, file)
 
-else:
-    print(f'Error: {response.status_code} {data.get("message", "no message found")}')
+        print(f'File {filename} was succesfully created.')
+        break
+
+    elif response.status_code >= 500:
+        time.sleep(10)
+        count += 1
+        print(f'Trying again. Attempt {count}.')
+
+    else:
+        print(f'Error: {response.status_code} {data.get("message", "no message found")}')
+        break
